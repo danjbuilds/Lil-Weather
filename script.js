4d0165e3a367552b32a320b7b97b690e1675503c
Dan J. Pasuengos <danjbuilds@users.noreply.github.com>
2026-05-02 22:19:18 +0800

    added weather card display fetch from api

--- script.js
+++ script.js
@@ -33,10 +33,47 @@ weatherForm.addEventListener("submit", async event => {
 
 async function getWeatherData(city){
 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+    console.log(response);
+
+    if(!response.ok){
+        throw new Error("Could not fetch weather data");
+    }
+    return await response.json();
+
 }
 
 function displayWeatherInfo(data){
+    console.log(data);
 
+    const {name: city,
+         main: {temp, humidity},
+          weather: [{description, id}] } = data;
+    
+    card.textContent = "";
+    card.style.display = "flex";
+
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+    const weatherEmoji = document.createElement("p");
+
+    const kTemp = temp;
+    const cTemp = (kTemp - 273.15).toFixed(1);
+
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${cTemp}°C`;
+    humidityDisplay.textContent = `${humidity}%`;
+    
+
+    card.appendChild(cityDisplay)
+    card.appendChild(tempDisplay)
+    card.appendChild(humidityDisplay)
+    card.appendChild(descDisplay)
+    
 }
 
 function getWeatherEmoji(weatherId){
