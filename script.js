b464269992a1c956aafa0cac37c921a09231a225
Dan J. Pasuengos <danjbuilds@users.noreply.github.com>
2026-05-03 13:59:30 +0800

    finished the first working version (from tutorial)

--- script.js
+++ script.js
@@ -66,17 +66,59 @@ function displayWeatherInfo(data){
 
     cityDisplay.textContent = city;
     tempDisplay.textContent = `${cTemp}°C`;
-    humidityDisplay.textContent = `${humidity}%`;
-    
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+    weatherEmoji.textContent = getWeatherEmoji(id);
 
+    descDisplay.classList.add('descDisplay');
+    weatherEmoji.classList.add('weatherEmoji');
+    cityDisplay.classList.add('cityDisplay');
+    tempDisplay.classList.add('tempDisplay');
+    humidityDisplay.classList.add('humidityDisplay');
+
+   
+    
+    
     card.appendChild(cityDisplay)
     card.appendChild(tempDisplay)
     card.appendChild(humidityDisplay)
     card.appendChild(descDisplay)
+    card.appendChild(weatherEmoji)
     
 }
 
 function getWeatherEmoji(weatherId){
+
+    switch (true) {
+    case (weatherId >= 200 && weatherId < 300):
+        return "⛈️";
+    case (weatherId >= 300 && weatherId <= 321):
+        return "🌦️";
+    case (weatherId >= 500 && weatherId <= 504):
+        return "🌧️";
+    case (weatherId === 511):
+        return "❄️🌧️";
+    case (weatherId >= 520 && weatherId <= 531):
+        return "🌧️";
+    case (weatherId >= 600 && weatherId <= 622):
+        return "❄️";
+    case (weatherId >= 701 && weatherId <= 781):
+        return "🌫️";
+    case (weatherId === 800):
+        return "☀️"; // Clear sky
+    case (weatherId === 801):
+        return "🌤️"; // Few clouds (Mostly Sunny)
+    case (weatherId === 802):
+        return "⛅"; // Scattered clouds (Partly Cloudy)
+    case (weatherId === 803):
+        return "🌥️"; // Broken clouds
+    case (weatherId === 804):
+        return "☁️"; // Overcast clouds (True Cloudy)
+  
+    default:
+        return "?";
+}
+
 
 }
 function displayError(message){
